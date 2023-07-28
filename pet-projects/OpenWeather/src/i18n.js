fe675ae9d847c1946248a03b4d5aa657f97868d2
ukrshadow <guslistyj@i.ua>
2023-07-28 16:19:26 +0300

    bugfix locales

--- pet-projects/OpenWeather/src/i18n.js
+++ pet-projects/OpenWeather/src/i18n.js
@@ -3,6 +3,7 @@ import {initReactI18next} from 'react-i18next';
 import languagedetector from 'i18next-browser-languagedetector';
 import backend from 'i18next-http-backend';
 const getCityNameFromStore = JSON.parse(localStorage.getItem('weather')).state.currentLanguage
+const path = ('path');
 
 i18n
 .use(backend)
@@ -12,10 +13,24 @@ i18n
     directory: path.join(__dirname, '/locales')
 }) */
 .init({
-    debug: true,
+    debug: false,
     fallback: 'en',
-    lng: getCityNameFromStore === undefined ? 'en': getCityNameFromStore,
-    loadPath: "./assets/locales/{{lng}}/{{ns}}.json"
+    /* lng: getCityNameFromStore === undefined ? 'en': getCityNameFromStore, */
+    locales: ['en', 'ua', 'he'],
+    defaultLocale: 'en',
+    autoReload: true,
+    extension: '.json',
+    /* directory:'./assets/public/locales', 
+    loadPath: "./assets/locales/{{lng}}/{{ns}}.json", */
+    
 });
 
+//const i18n = new I18n({
+//    locales: ['en', 'zh-TW'],
+//    defaultLocale: 'en',
+//    autoReload: true,
+//    extension: '.json',
+//    directory: path.join('./src', 'locales')
+// });
+ 
 export default i18n
