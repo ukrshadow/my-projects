400073794a6ec314d3f36cfd3884df810d554b30
ukrshadow <guslistyj@i.ua>
2023-07-28 17:06:49 +0300

    bugfix locales

--- pet-projects/OpenWeather/src/i18n.js
+++ pet-projects/OpenWeather/src/i18n.js
@@ -2,38 +2,25 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import languagedetector from 'i18next-browser-languagedetector';
 import backend from 'i18next-http-backend';
+
 const getCityNameFromStore = JSON.parse(localStorage.getItem('weather')).state.currentLanguage
-const path = ('path');
 
 i18n
 .use(backend)
 .use(languagedetector)
 .use(initReactI18next)
-/* .configure({
-    directory: path.join(__dirname, '/locales')
-}) */
 .init({
-    debug: false,
+    debug: true,
     fallback: 'en',
-    /* lng: getCityNameFromStore === undefined ? 'en': getCityNameFromStore, */
-    locales: ['en', 'ua', 'he'],
-    defaultLocale: 'en',
-    autoReload: true,
-    extension: '.json',
-    interpolation : {
+    lng: getCityNameFromStore === undefined ? 'en': getCityNameFromStore,
+    defaultLanguage: 'en',
+    interpolation: {
         escapeValue: false
+      },
+    /* directory:('./locales'),  */
+    backend: {
+        loadPath: '/locales/{{lng}}/{{ns}}.json'
     },
-    directory:'./locales', 
-    loadPath: `./locales/{{lng}}/{{ns}}.json`,
-    
 });
 
-//const i18n = new I18n({
-//    locales: ['en', 'zh-TW'],
-//    defaultLocale: 'en',
-//    autoReload: true,
-//    extension: '.json',
-//    directory: path.join('./src', 'locales')
-// });
- 
 export default i18n
